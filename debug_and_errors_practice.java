05aeb5d507cc28ec0474c7a29964bcedc80aaeeb
Datboi-Dan <datboidan961@gmail.com>
2024-08-16 18:23:34 -1000

    Create debug_and_errors_practice.java

---
+++ debug_and_errors_practice.java
@@ -0,0 +1,28 @@
+/*
+  For each task below, explain why the code is incorrect, what type of error that is, and how to fix it.
+*/
+
+//Task 1: Print out "Hello World"
+System.out.print("Hello World);
+//Argument is missing a quotation mark at the end. To fix the error, add the quotation mark after Hello World to make it a string.
+
+//Task 2: Print out "Hello World"
+System.out.print(Hello World);
+//Argument is not surrounded by quotation marks. To fix the error, either surround Hello World in quotes to make it a string, or declare a variable called Hello World bfore line 10.
+
+//Task 3: Print out "Hello World"
+System.Out.Print("Hello World");
+//Java is case sensitive, and does not recognize the function due to the capitalized O and P in System.Out.Print. To fix the error, make the O and P lowercase so that it reads as System.out.print so the computer reads it.
+
+//Task 4: Print out "Hello World"
+System.out.print("Hello World")
+//Missing a semicolon at the end to end the function. Without to semicolon, the computer doesn't know where to stop reading, so to fix the error, just put a semicolon at the end of the line.
+
+//Task 5: Print out "Hello World"
+System.out.print("Hello Wrold")
+//The task was to print out "Hello World", not "Hellow Wrold". To fix it, just spell World correctly.
+
+//Task 6: Print out "Hello World twice, one on top of the other"
+System.out.print("Hello World");
+System.out.print("Hello World");
+//To print on another line, ln must be added to the preceding print function. To fix the error, add ln after print in line 26.
