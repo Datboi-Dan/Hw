3166585f9c074e4829c28cecdab85ad3902bc3c8
Datboi-Dan <datboidan961@gmail.com>
2024-10-22 21:02:58 -1000

    Update StringPracticeTwo.java

--- CodingBatSolns/StringPracticeTwo.java
+++ CodingBatSolns/StringPracticeTwo.java
@@ -2,6 +2,15 @@
 public class StringPracticeTwo
 {
 	
+	/**
+	 * Challenge 1: makeOutWord.
+	 * Given an "out" string length 4, such as "<<>>", and a word, return a new string where the word is in the middle of the out string.
+	 * e.g. "<<word>>" 
+	 * Note: use str.substring(i, j) to extract the String starting at index i and going up to but not including index j.
+	 * @param out - The container surrounding the word.
+	 * @param word = The word to be surrounded.
+	 * @return The word surrounded by out.
+	 */
 	public String makeOutWord(String out, String word) 
 	{
 	  
@@ -10,6 +19,13 @@ public class StringPracticeTwo
 	}
 	
 	
+	/**
+	 * Challenge 2: extraEnd.
+	 * Given a string, return a new string made of 3 copies of the last 2 chars of the original string. 
+	 * The string length will be at least 2.
+	 * @param str - The string that will have its last two characters copied. 
+	 * @return The last two characters of the string three times in a row.
+	 */
 	public String extraEnd(String str) 
 	{
 	  
@@ -18,6 +34,14 @@ public class StringPracticeTwo
 	}
 	
 	
+	/**
+	 * Challenge 3: firstTwo.
+	 * Given a string, return the string made of its first two chars, so the String "Hello" yields "He". 
+	 * If the string is shorter than length 2, return whatever there is, so "X" yields "X", and the empty string "" yields the empty string "". 
+	 * Note that str.length() returns the length of a string.
+	 * @param str the string to be cut to its first two characters.
+	 * @return Either the strings first two characters or the string itself if its length is less than two.
+	 */
 	public String firstTwo(String str) 
 	{
 	  
@@ -27,6 +51,13 @@ public class StringPracticeTwo
 	}
 	
 	
+	/**
+	 * Challenge 4: withoutEnd.
+	 * Given a string, return a version without the first and last char, so "Hello" yields "ell". 
+	 * The string length will be at least 2.
+	 * @param str The string to be cut to its middle characters.
+	 * @return The string without its first and last characters.
+	 */
 	public String withoutEnd(String str) 
 	{
 	  
