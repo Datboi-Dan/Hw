b5598314fff3db6052bd64667e53e2ab3dad9e44
Datboi-Dan <datboidan961@gmail.com>
2024-12-04 00:57:02 -1000

    Update StringsPracticeForProject.java
    Added more documentation and fixed logic error in calculating numbers after the decimal point, as well as in detecting invalid strings.

--- StringsPracticeForProject.java
+++ StringsPracticeForProject.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 /**
  * This class is a submission to the Strings Practice (for Project) assignment.
  * I made sure not to use anything we didn't learn in class. (I think)
+ * I made things so much harder than they needed to be though in making a string to double converter. (So we can prevent an error from terminating the program)
  * MILCS
  * @author Daniel B)
  */
@@ -30,7 +31,7 @@ public class StringsPracticeForProject
 		{
 			
 			System.out.println("Iteration " + iteration++ + ": " + input); //Prints out iteration, increments it, then prints out the input.
-			System.out.println("Please enter a new input, or input stop."); //Prompts for a new input, or stop.
+			System.out.println("Please enter a new input, or input stop."); //Prompts for a new input.
 			input = myReader.nextLine(); //Reads whatever user inputs next and sets it to input.
 			
 		}
@@ -44,14 +45,20 @@ public class StringsPracticeForProject
 		 * After the loop is done, print the shape's original attribute value and its final attribute value. 
 		 */
 		
+		
 		Sphere sphere1 = new Sphere(1); //Instantiates a sphere with a radius of 1.0. (1 is implicitly cast to a double.)
 		
 		for (int i = 0; i < 5; i++) //Iterates 5 times.
 		{
 			
-			System.out.println("Please enter a double.");
-			input = myReader.nextLine();
 			
+			System.out.println("Please enter a double."); //Prompt for a double.
+			input = myReader.nextLine(); //Reads whatever user inputs next and sets it to input.
+			String inputCopy = input;
+			
+			//Check to see if input is a double to avoid error and halt of code.
+			//Unfortunately this doesn't work for decimals past 14 digits after the decimal point (I checked myself cause I was curious).
+			//Also Math.pow sometimes doesn't work for some reason meaning this can be inconsistent?
 			boolean isNegative = false;
 			
 			while (!checkIfDouble(input)) //Iterates until the input is a valid double.
@@ -71,22 +78,44 @@ public class StringsPracticeForProject
 				
 			}
 			
-			for (int j = 0; j < input.length(); j++) //Iterate through the input add or subtract by iterating through each char, converting it to an int, then multiplying it by a power of 10 depending on j.
+			for (int j = 0; j < input.length(); j++) //Iterate through the input and add or subtract by iterating through each char, converting it to an int, then multiplying it by a power of 10 depending on the index of the decimal point - 1 - j.
 			{
 				
-				if (input.charAt(j) != '.')
+				if (input.charAt(j) != '.') //Skips body if the character is the decimal point.
 				{
 					
-					if (isNegative)
+					if (isNegative) //Subtracts if the decimal is negative.
 					{
 						
-						sphere1.setRadius(sphere1.getRadius() - ( convertCharToInt(input.charAt(j)) * Math.pow(10, input.indexOf('.') - j - 1) ));
+						if (inputCopy.indexOf(input.charAt(j)) > input.indexOf('.')) //Subtracts an extra one from the power if greater than zero.
+						{
+							
+							sphere1.setRadius(sphere1.getRadius() - (convertCharToInt(input.charAt(j)) * Math.pow(10, input.indexOf('.') - j)));
+							
+						}
+						else
+						{
+							
+							sphere1.setRadius(sphere1.getRadius() - (convertCharToInt(input.charAt(j)) * Math.pow(10, input.indexOf('.') - j - 1)));
+							
+						}
 					
 					}
-					else
+					else //Adds if it is not.
 					{
 						
-						sphere1.setRadius(sphere1.getRadius() + ( convertCharToInt(input.charAt(j)) * Math.pow(10, input.indexOf('.') - j - 1) ));
+						if (inputCopy.indexOf(input.charAt(j)) > input.indexOf('.')) //Subtracts an extra one from the power if greater than zero.
+						{
+							
+							sphere1.setRadius(sphere1.getRadius() + (convertCharToInt(input.charAt(j)) * Math.pow(10, input.indexOf('.') - j)));
+							
+						}
+						else
+						{
+							
+							sphere1.setRadius(sphere1.getRadius() + (convertCharToInt(input.charAt(j)) * Math.pow(10, input.indexOf('.') - j - 1)));
+							
+						}
 						
 					}
 					
@@ -96,12 +125,17 @@ public class StringsPracticeForProject
 			
 		}
 		
-		System.out.print(sphere1.getRadius());
+		System.out.print("New Radius: " + sphere1.getRadius()); //Prints the new Radius.
 	
 		myReader.close();
 	}
 	
 	
+	/**
+	 * Method to convert a char to an int.
+	 * @param character - The character to be converted to an int.
+	 * @return the character as an int, or -1 if the int could not be converted.
+	 */
 	public static int convertCharToInt(char character)
 	{
 		
@@ -174,15 +208,21 @@ public class StringsPracticeForProject
 		
 	}
 	
+	/**
+	 * Counts the number of occurrences a character has in a string.
+	 * @param string - The string that is checked for the character.
+	 * @param character - The string whose occurrences are counted in the passed string.
+	 * @return The amount of times the passed character occurs in the passed string.
+	 */
 	public static int countOccurrences(String string, char character)
 	{
 		
-		int counter = 0;
+		int counter = 0; //Counter to keep track of character occurrence.
 		
-		for (int i = 0; i < string.length(); i++)
+		for (int i = 0; i < string.length(); i++) //Iterate through the string to count the occurrences.
 		{
 			
-			if (string.charAt(i) == character)
+			if (string.charAt(i) == character) //Increments the counter if the current character is equal to the passed character.
 			{
 				
 				counter++;
@@ -191,15 +231,19 @@ public class StringsPracticeForProject
 			
 		}
 		
-		return counter;
+		return counter; //Returns the counter.
 		
 	}
 	
-	
+	/**
+	 * Checks if the passed input is a double or not.
+	 * @param input - the string to be checked as a double or not.
+	 * @return A boolean representing whether or not the String is a valid double or not.
+	 */
 	public static boolean checkIfDouble(String input)
 	{
 		
-		for (int j = 0; j < input.length(); j++) //Iterate through the input to check if the input is a valid double.
+		for (int j = 0; j < input.length(); j++) //Iterate through the input to check if all the characters are valid.
 		{
 			
 			if 
@@ -222,46 +266,48 @@ public class StringsPracticeForProject
 			)
 			{
 				
-				return false;
+				return false; //Returns false if there is an invalid character in the string.
 				
 			}
 				
 		}
 			
-		if 
+		if //More checks.
 		(
 					
-			input.length() < 3
+			//Checks if the input length is less than three.
+			input.length() < 3 
 			
 			||
-					
-			(input.charAt(0) != '0' && input.charAt(1) != '.') &&
-			input.charAt(0) != '1' && 
-			input.charAt(0) != '2' &&
-			input.charAt(0) != '3' && 
-			input.charAt(0) != '4' && 
-			input.charAt(0) != '5' && 
-			input.charAt(0) != '6' &&
-			input.charAt(0) != '7' &&
-			input.charAt(0) != '8' &&
-			input.charAt(0) != '9' &&
-			input.charAt(0) != '-'
-					
-					
+			
+			//Checks if the first character is the decimal point.
+			input.charAt(0) == '.'
+			
+			||
+			
+			//Checks if the first character is 0, and if it is, it is only valid if there is a decimal point after
+			input.charAt(0) == '0' && input.charAt(1) != '.'
+			
+			||
+			
+			//Same check but with a negative sign.
+			input.charAt(0) == '-' && (input.charAt(1) == '.' || (input.charAt(1) == '0' && input.charAt(2) != '.'))
+						
 			||
 					
+			//Checks if there is only one decimal point in the string.
 			countOccurrences(input, '.') != 1
 					
 		)
 		{
 				
-			return false;
+			return false; //Returns false if any of the the previous are true.
 				
 		}
 		else
 		{
 				
-			return true;
+			return true; //Returns true if all of the checks are passed, meaning it is a valid double.
 				
 		}
 		
